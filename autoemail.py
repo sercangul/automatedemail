77472ba44bfaf08ab5f35e5dd785297946a3d833
Sercan Gul <sercan.gul@gmail.com>
2021-05-12 20:42:41 -0500

    Update autoemail.py

--- autoemail.py
+++ autoemail.py
@@ -38,7 +38,6 @@ df = pd.read_csv('2021_contact_list_for_script.csv')
 
 #df = pd.read_csv('script-test.csv') 
 
-
 #------------------------------------------------------------------------------
 for index, row in df.iterrows():
     Name = row['Name']
@@ -49,7 +48,6 @@ for index, row in df.iterrows():
     message["From"] = sender_email
     message["Subject"] = subject
     message["To"] = email
-    
     
     body = "Dear %s \n \nThe Graduate Engineering Council (GEC) welcomes you to our first virtual GAIN (Graduate and Industry Networking) event since its inception in 2004. GAIN facilitates connections between employers and our outstanding graduate students through student presentations, industry talks, and a career fair.\n\nIf you would like to learn more about the cutting-edge research conducted at UT Austin, as well as recruit some of the brightest graduate engineers in the nation, then this event is for you. GAIN 2021 will be held virtually on February 25, 2021! Registration is now open, and we would love to have %s participate.\n\nPlease see the links below to access the itinerary, the updated corporate package with pricing, and the registration. \nItinerary: https://sites.utexas.edu/gain/about/itinerary/  \nCorporate package: https://sites.utexas.edu/gain/home/partnership/ \nRegistration: https://tinyurl.com/yyrzdpy3 \n\nSince the event will be virtual, our expenses will also be lower. Therefore, there is a good discount on sponsorship prices compared to previous years.\nYou can find more about GAIN through our website at https://sites.utexas.edu/gain/. \n\nSincerely, \nSheila Gerardo & Sercan Gul \nGAIN Co-Directors, 2021 \nGAIN Website: https://sites.utexas.edu/gain/ "   % (Name, Company)    
     message.attach(MIMEText(body, "plain"))
